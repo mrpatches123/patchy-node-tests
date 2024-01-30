5ab557f792d99e1aed496ef411753ddde483f3fa
mrpatches123 <49619048+mrpatches123@users.noreply.github.com>
2024-01-30 11:44:30 -0500

    wdwwd

--- random_cirialar_coords.js
+++ random_cirialar_coords.js
@@ -8,6 +8,7 @@ function circularToCartesian(vectorCircular, center = { x: 0, z: 0 }) {
 	const { x, z } = center;
 	return ({ x: radius * Math.cos(radians) + x, z: radius * Math.sin(radians) + z, theta: radians, r: radius });
 }
+const oneRotation = Math.PI * 2;
 /**
  * @param {{x: number, z: number}} vector 
  * @param {{x: number, z: number}} center 
@@ -17,9 +18,13 @@ function cartesianToCircular(vector, center = { x: 0, z: 0 }) {
 	const { x, z } = vector;
 	const { x: xc, z: zc } = center;
 	const r = Math.sqrt((x - xc) ** 2 + (z - zc) ** 2);
-	return ({ theta: Math.acos((x - xc) / r), r, x, z });
+	let theta = Math.atan2(z - zc, x - xc);
+	console.log(theta);
+	if (theta < 0) theta = oneRotation + theta;
+	return ({ theta, r, x, z });
 }
-const oneRotation = 2 * Math.pi();
+console.log(cartesianToCircular({ x: -1, z: -0.1 }));
+
 /**
  * @param {number} minR 
  * @param {number} maxR 
@@ -29,14 +34,15 @@ const oneRotation = 2 * Math.pi();
  */
 function getCircularRandomRangeOppositeFromVectors(minR, maxR, vectors = [], center = { x: 0, z: 0 }) {
 	let vectorsInRange = vectors.map((vector) => cartesianToCircular(vector, center))
-		.filter(({ r }) => r >= minR && r <= maxR).sort(({ thetaA }, { thetaB }) => thetaA - thetaB);
+		.filter(({ r }) => r >= minR && r <= maxR).sort(({ theta: thetaA }, { theta: thetaB }) => thetaA - thetaB);
+	console.log(vectorsInRange);
 	if (!vectorsInRange.length) return { minR, maxR, mintheta: 0, maxtheta: oneRotation };
 	else if (vectorsInRange.length > 2) {
 		const AjectentVectors = [];
 		for (let a = 0, b = 1; b < vectorsInRange.length; a++, b++) {
-			if (b === vectorsInRange.length) {
+			if (b === vectorsInRange.length - 1) {
 				const aV = vectorsInRange[0];
-				const bV = vectorsInRange[a];
+				const bV = vectorsInRange[b];
 				const test = aV > bV;
 				AjectentVectors.push([(test) ? bV : aV, (test) ? aV : bV, Math.abs(aV.theta - bV.theta)]);
 				continue;
@@ -48,7 +54,8 @@ function getCircularRandomRangeOppositeFromVectors(minR, maxR, vectors = [], cen
 			AjectentVectors.push([(test) ? bV : aV, (test) ? aV : bV, Math.abs(aV.theta - bV.theta)]);
 		}
 		const [minVector, maxVector] = AjectentVectors.sort((a, b) => b[2] - a[2])[0];
-		return { minR, maxR, mintheta: oneRotation - maxVector.theta, maxtheta: oneRotation - minVector.theta };
+		console.log({ minVector, maxVector });
+		return { minR, maxR, mintheta: minVector.theta, maxtheta: maxVector.theta };
 	} else if (vectorsInRange.length === 2) {
 		const [aV, bV] = vectorsInRange;
 		const test = aV > bV;
@@ -59,5 +66,5 @@ function getCircularRandomRangeOppositeFromVectors(minR, maxR, vectors = [], cen
 	}
 	console.log(vectorsInRange);
 }
-const players = [{ x: 22, z: 22 }, { x: 1, z: -5 }, { x: -8, z: -5 }];
-getCircularRandomRangesFromVectors(0, 10, players);
+const players = [{ x: 2, z: -4 }, { x: 3, z: -5 }, { x: -8, z: -3 }, { x: -2, z: 5 }, { x: 1, z: 5 }];
+console.log(getCircularRandomRangeOppositeFromVectors(0, 100, players));
