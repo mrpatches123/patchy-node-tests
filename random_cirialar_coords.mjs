1b49b67202c2a688abd8d34b4ef4f139f2439cee
mrpatches123 <49619048+mrpatches123@users.noreply.github.com>
2024-02-01 11:58:36 -0500

    weefew

--- random_cirialar_coords.mjs
+++ random_cirialar_coords.mjs
@@ -1,4 +1,5 @@
-
+import path from 'path';
+import fs from 'fs';
 /**
  * @param {{r: number, theta: number}} radians 
  * @param {{x: number, z: number}} center 
@@ -12,7 +13,7 @@ const oneRotation = Math.PI * 2;
 /**
  * @param {{x: number, z: number}} vector 
  * @param {{x: number, z: number}} center 
- * @returns {x: number, z: number, theta: number, r: number}
+ * @returns {{x: number, z: number, theta: number, r: number}}
  */
 function cartesianToCircular(vector, center = { x: 0, z: 0 }) {
 	const { x, z } = vector;
@@ -42,6 +43,7 @@ function getCircularRandomRangeOppositeFromVectors(minR, maxR, vectors = [], cen
 		for (let a = 0, b = 1; b < vectorsInRange.length; a++, b++) {
 			if (b === vectorsInRange.length - 1) {
 				const aV = vectorsInRange[0];
+				aV.theta += oneRotation;
 				const bV = vectorsInRange[b];
 				const test = aV > bV;
 				AjectentVectors.push([(test) ? bV : aV, (test) ? aV : bV, Math.abs(aV.theta - bV.theta)]);
@@ -55,7 +57,9 @@ function getCircularRandomRangeOppositeFromVectors(minR, maxR, vectors = [], cen
 		}
 		const [minVector, maxVector] = AjectentVectors.sort((a, b) => b[2] - a[2])[0];
 		console.log({ minVector, maxVector });
-		return { minR, maxR, mintheta: minVector.theta, maxtheta: maxVector.theta };
+		const mintheta = Math.min(minVector.theta, maxVector.theta);
+		const maxtheta = Math.max(minVector.theta, maxVector.theta);
+		return { minR, maxR, mintheta: mintheta + mintheta / 8, maxtheta: maxtheta - mintheta / 8, center };
 	} else if (vectorsInRange.length === 2) {
 		const [aV, bV] = vectorsInRange;
 		const test = aV > bV;
@@ -66,5 +70,41 @@ function getCircularRandomRangeOppositeFromVectors(minR, maxR, vectors = [], cen
 	}
 	console.log(vectorsInRange);
 }
-const players = [{ x: 2, z: -4 }, { x: 3, z: -5 }, { x: -8, z: -3 }, { x: -2, z: 5 }, { x: 1, z: 5 }];
-console.log(getCircularRandomRangeOppositeFromVectors(0, 100, players));
+function randomInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+const players = Array.from(new Array(10), () => ({ x: randomInt(-10, 10), z: randomInt(-10, 10) }));
+const test = getCircularRandomRangeOppositeFromVectors(0, 100, players);
+console.log(test);
+const graphThing = `<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+	<script src="https://www.desmos.com/api/v1.8/calculator.js?apiKey=dcb31709b452b1cf9dc26972add0fda6"></script>
+
+
+
+</head>
+
+<body>
+	<div id="calculator" style="width: 700px; height: 400px;"></div>
+
+	<script>
+		var elt = document.getElementById('calculator');
+		var calculator = Desmos.GraphingCalculator(elt);
+		calculator.setExpression({ id: 'graph1', latex: '${players.map(({ x, z }) => `(${x},${z})`).join(',')}' });
+		calculator.setExpression({ id: 'graph2', latex: 'r=5 {{${test.mintheta}<\\\\theta<${test.maxtheta}}}'});
+
+	</script>
+
+
+</body>
+
+</html>`;
+const htmlPath = path.resolve('./graph.html');
+console.log(htmlPath);
+fs.writeFileSync(htmlPath, graphThing);
